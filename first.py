b60f63a6207bdb41eedcb631a55c383f89d3eb4c
Rohit55-cy <kushwahrohit5665@gmail.com>
2025-04-17 19:19:26 +0530

    Add new first Python file

---
+++ first.py
@@ -0,0 +1,17 @@
+import random  
+
+# Generate a random number between 1 and 100  
+secret_number = random.randint(1, 100)  
+
+# Let the user guess  
+attempts = 0  
+while True:  
+    guess = int(input("Guess a number between 1 and 10: "))  
+    attempts += 1  
+    if guess < secret_number:  
+        print("Too low! Try again.")  
+    elif guess > secret_number:  
+        print("Too high! Try again.")  
+    else:  
+        print(f"Congratulations! You guessed it in {attempts} attempts! 🎉")  
+        break  
